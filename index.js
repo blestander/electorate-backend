0ff94893ef59e0987ef3b9fe3aa2772e93e2e5a0
Brendan Lestander <blestander@pitt.edu>
2020-04-21 17:21:34 -0400

    Handled CORS on token function.

--- index.js
+++ index.js
@@ -4,5 +4,21 @@ exports.helloHTTP = (request, response) => {
 };
 
 exports.token = (request, response) => {
-    response.status(200).send(request.body)
+    if (handleCORS(request, response, ["POST"]))
+        response.status(200).send(request.body)
+}
+
+function handleCORS(request, response, methods) {
+    response.set('Access-Control-Allow-Origin', '*')
+
+    if (request.method === "OPTIONS") {
+        response.set('Access-Control-Allow-Methods', methods.join(','))
+        response.set('Access-Control-Allow-Headers', 'Content-Type')
+        response.status(204).send('')
+        return false;
+    } else if (!(methods.includes(request.method))) {
+        response.status(405).send('')
+        return false;
+    } else
+        return true;
 }
