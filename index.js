abccef9a8266102c69fadc454b48cf512e54db43
Brendan Lestander <blestander@pitt.edu>
2020-04-21 18:13:49 -0400

    obtainToken now retrieves an actual token from Discord.

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
+const superagent = require("superagent");
+
+const DISCORD_OBTAIN_TOKEN_URL = "https://discordapp.com/api/oauth2/token"
+const DISCORD_SCOPE = encodeURI("identify guild")
+const DISCORD_REDIRECT_URI = "http://localhost:4200/auth"
 
 exports.helloHTTP = (request, response) => {
     response.send("Hello, world!")
 };
 
 exports.obtainToken = (request, response) => {
-    if (handleCORS(request, response, ["POST"]))
-        response.status(200).send(request.body)
+    if (handleCORS(request, response, ["POST"])) {
+        superagent.post(DISCORD_OBTAIN_TOKEN_URL)
+            .send(`client_id=${process.env.CLIENT_ID}`)
+            .send(`client_secret=${process.env.CLIENT_SECRET}`)
+            .send(`scope=${DISCORD_SCOPE}`)
+            .send(`redirect_uri=${DISCORD_REDIRECT_URI}`)
+            .send("grant_type=authorization_code")
+            .send(`code=${request.body.code}`)
+            .then((dis_response) => {
+                response.status(200).send(dis_response.body)
+            });
+    }
 }
 
 function handleCORS(request, response, methods) {
