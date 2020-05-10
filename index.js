a93aa7b579a6c3c68f1437957cf408e061c463e8
Brendan Lestander <blestander@pitt.edu>
2020-05-10 14:38:24 -0400

    Turned handleCORS into closure.

--- index.js
+++ index.js
@@ -9,46 +9,45 @@ exports.helloHTTP = (request, response) => {
     response.send("Hello, world!")
 };
 
-exports.obtainToken = (request, response) => {
-    if (handleCORS(request, response, ["POST"])) {
-        if (!("code" in request.body)) {
-            response.status(400).send({error: "no_code"})
-            return;
-        }
-        superagent.post(DISCORD_OBTAIN_TOKEN_URL)
-            .send(`client_id=${process.env.CLIENT_ID}`)
-            .send(`client_secret=${process.env.CLIENT_SECRET}`)
-            .send(`scope=${DISCORD_SCOPE}`)
-            .send(`redirect_uri=${DISCORD_REDIRECT_URI}`)
-            .send("grant_type=authorization_code")
-            .send(`code=${request.body.code}`)
-            .then((dis_response) => {
-                response.status(200).send({
-                    access_token: dis_response.body.access_token,
-                    refresh_token: dis_response.body.refresh_token,
-                    expires_on: moment().add(dis_response.body.expires_in, 'seconds').format(),
-                    scope: dis_response.body.scope
-                })
-            }).catch(error => {
-                if (error.response.body.error == "invalid_request")
-                    response.status(400).send({error: "invalid_code"})
-                else
-                    response.status(error.status).send(error.response.body)
-            });
+exports.obtainToken = handleCORS((request, response) => {
+    if (!("code" in request.body)) {
+        response.status(400).send({error: "no_code"})
+        return;
     }
-}
+    superagent.post(DISCORD_OBTAIN_TOKEN_URL)
+        .send(`client_id=${process.env.CLIENT_ID}`)
+        .send(`client_secret=${process.env.CLIENT_SECRET}`)
+        .send(`scope=${DISCORD_SCOPE}`)
+        .send(`redirect_uri=${DISCORD_REDIRECT_URI}`)
+        .send("grant_type=authorization_code")
+        .send(`code=${request.body.code}`)
+        .then((dis_response) => {
+            response.status(200).send({
+                access_token: dis_response.body.access_token,
+                refresh_token: dis_response.body.refresh_token,
+                expires_on: moment().add(dis_response.body.expires_in, 'seconds').format(),
+                scope: dis_response.body.scope
+            })
+        }).catch(error => {
+            if (error.response.body.error == "invalid_request")
+                response.status(400).send({error: "invalid_code"})
+            else
+                response.status(error.status).send(error.response.body)
+        });
+}, ["POST"]);
 
-function handleCORS(request, response, methods) {
-    response.set('Access-Control-Allow-Origin', '*')
+function handleCORS(func, methods) {
+    return function(request, response) {
+        response.set('Access-Control-Allow-Origin', '*')
 
-    if (request.method === "OPTIONS") {
-        response.set('Access-Control-Allow-Methods', methods.join(','))
-        response.set('Access-Control-Allow-Headers', 'Content-Type')
-        response.status(204).send('')
-        return false;
-    } else if (!(methods.includes(request.method))) {
-        response.status(405).send('')
-        return false;
-    } else
-        return true;
+        if (request.method === "OPTIONS") {
+            response.set('Access-Control-Allow-Methods', methods.join(','))
+            response.set('Access-Control-Allow-Headers', 'Content-Type')
+            response.status(204).send('')
+            return false;
+        } else if (!(methods.includes(request.method))) {
+            response.status(405).send('')
+        } else
+            func(request, response);
+    }
 }
