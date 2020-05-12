d901d8c42d09cc4c0ddc0dd80065028895e8479d
Brendan Lestander <blestander@pitt.edu>
2020-05-12 17:30:05 -0400

    Added rejection of already casted ballots.

--- castVote.js
+++ castVote.js
@@ -11,6 +11,7 @@ exports.castVote = handleCORS((request, response) => {
             let docRef = db.collection('polls').doc(poll_id);
             docRef.get()
                 .then(checkPollAndRequestBallot(response, user_id, choice, docRef))
+                .catch(err => response.status(500).send('Server error'))
         } else // Token is invalid
             response.status(403).send('Forbidden');
     } else // Something missing
@@ -24,17 +25,29 @@ function checkPollAndRequestBallot(response, user_id, choice, docRef) {
             if (docdata.finished) // Poll is concluded
                 response.status(409).send({
                     error: "finished"
-                })
+                });
             else if (!docdata.options.includes(choice))
                 response.status(409).send({
                     error: "invalid choice"
-                })
-            else
-                response.status(200).send({
-                    id: user_id,
-                    choice: choice
                 });
+            else {
+                let ballotsRef = docRef.collection('ballots');
+                ballotsRef.where('voter', '==', user_id)
+                    .get().then(checkBallotAndCreateBallot(response, user_id, choice, docRef))
+                    .catch(err => response.status(500).send('Server error'));
+            }
         } else // Poll does not exist
             response.status(404).send('Not found');
     };
+}
+
+function checkBallotAndCreateBallot(response, user_id, choice, ballotRef) {
+    return ballot => {
+        if (ballot.size) // Vote already cast
+            response.status(409).send({
+                error: "already voted"
+            });
+        else
+            response.status(200).send({choice: choice});
+    }
 }
