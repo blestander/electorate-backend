d64f78d00207db4a7474b394aa7aee2efea04540
Brendan Lestander <blestander@pitt.edu>
2020-05-12 16:59:15 -0400

    castVote() now requests the poll from the database

--- castVote.js
+++ castVote.js
@@ -8,12 +8,20 @@ exports.castVote = handleCORS((request, response) => {
             let user_id = token.id;
             let poll_id = request.body.id;
             let choice = request.body.choice;
-            response.send(200).send({
-                id: poll_id,
-                choice: choice
-            })
+            let docRef = db.collection('polls').doc(poll_id);
+            docRef.get()
+                .then(checkPollAndRequestBallot(response, user_id, choice, docRef))
         } else // Token is invalid
             response.status(403).send('Forbidden');
     } else // Something missing
         response.status(400).send('Bad request');
 }, ["POST"]);
+
+function checkPollAndRequestBallot(response, user_id, choice, docRef) {
+    return doc => {
+        response.status(200).send({
+            id: user_id,
+            choice: choice
+        });
+    };
+}
