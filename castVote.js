6191770838ce799fd82e0887871c6d48d9a17566
Brendan Lestander <blestander@pitt.edu>
2020-05-12 16:45:49 -0400

    Added token handling code.

--- castVote.js
+++ castVote.js
@@ -1,5 +1,14 @@
-const { handleCORS } = require("./utility.js");
+const { handleCORS, validateJWT } = require("./utility.js");
 
 exports.castVote = handleCORS((request, response) => {
-    response.status(200).send('Hello, world!');
+    if ("token" in request.body && "id" in request.body && "choice" in request.body) { // Nothing missing
+        if (validateJWT(request.body.token)) { // Token is valid
+            response.status(200).send({
+                id: request.body.id,
+                choice: request.body.choice
+            });
+        } else // Token is invalid
+            response.status(403).send('Forbidden');
+    } else // Something missing
+        response.status(400).send('Bad request');
 }, ["POST"]);
