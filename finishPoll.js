7d4322f012d4707a819debd603536ac8b4ae1769
Brendan Lestander <blestander@pitt.edu>
2020-05-14 10:50:45 -0400

    FinishPoll now ensures that the poll exists.

--- finishPoll.js
+++ finishPoll.js
@@ -1,10 +1,23 @@
 const { ensureLogin } = require('./utility.js');
+const { db } = require('./db.js');
 
 exports.finishPoll = ensureLogin(finishPollInternal);
 
 function finishPollInternal(request, response, token) {
     if ("id" in request.body) { // Everything present in request
-        response.status(200).send({});
+        let poll_id = request.body.id;
+        let user_id = token.id;
+        let docRef = db.collection("polls").doc(poll_id);
+        docRef.get().then(processPollAndRequestBallots(request, response, user_id, docRef));
     } else // Incomplete request
-        response.status(400).send('');
+        response.status(400).send('Bad request');
+}
+
+function processPollAndRequestBallots(request, response, user_id, docRef) {
+    return snapshot => {
+        if (snapshot.exists) { // This poll in fact exists
+            response.status(200).send('');
+        } else // No such poll exists
+            response.status(404).send("no_such_poll");
+    }
 }
