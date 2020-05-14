01a0178dcfa85c66cc6aa40e2868f0d5ef0bb381
Brendan Lestander <blestander@pitt.edu>
2020-05-14 10:59:13 -0400

    FinishPoll now catches nonowners and finished polls

--- finishPoll.js
+++ finishPoll.js
@@ -8,16 +8,33 @@ function finishPollInternal(request, response, token) {
         let poll_id = request.body.id;
         let user_id = token.id;
         let docRef = db.collection("polls").doc(poll_id);
-        docRef.get().then(processPollAndRequestBallots(request, response, user_id, docRef));
+        docRef.get()
+            .then(processPollAndRequestBallots(request, response, user_id, docRef))
+            .catch(err => response.status(500).send('Server error'));
     } else // Incomplete request
         response.status(400).send('Bad request');
 }
 
-function processPollAndRequestBallots(request, response, user_id, docRef) {
+function processPollAndRequestBallots(request, response, user_id, pollRef) {
     return snapshot => {
         if (snapshot.exists) { // This poll in fact exists
-            response.status(200).send('');
+            let poll = snapshot.data();
+            if (poll.owner == user_id) // If this user owns the poll, proceed
+                if (!poll.finished) { // Poll is still open
+                    pollRef.collection("ballot").get()
+                        .then(processBallotsAndSaveResults(request, response, pollRef, poll.method))
+                        .catch(err => response.status(500).send('Server error'));
+                } else
+                    response.status(409).send("finished");
+            else
+                response.status(403).send('Not owner');
         } else // No such poll exists
             response.status(404).send("no_such_poll");
+    }
+}
+
+function processBallotsAndSaveResults(request, response, pollRef, method) {
+    return snapshot => {
+        response.status(200).send({});
     }
 }
