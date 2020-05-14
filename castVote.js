c152ca4a1cad1ccb745f11818b4fa2d9a9c151d0
Brendan Lestander <blestander@pitt.edu>
2020-05-14 08:21:50 -0400

    Converted castVote.

--- castVote.js
+++ castVote.js
@@ -1,22 +1,18 @@
-const { handleCORS, validateJWT, decodeJWT } = require("./utility.js");
+const { ensureLogin, decodeJWT } = require("./utility.js");
 const { db } = require('./db.js')
 
-exports.castVote = handleCORS((request, response) => {
-    if ("token" in request.body && "id" in request.body && "choice" in request.body) { // Nothing missing
-        if (validateJWT(request.body.token)) { // Token is valid
-            let token = decodeJWT(request.body.token);
-            let user_id = token.id;
-            let poll_id = request.body.id;
-            let choice = request.body.choice;
-            let docRef = db.collection('polls').doc(poll_id);
-            docRef.get()
-                .then(checkPollAndRequestBallot(response, user_id, choice, docRef))
-                .catch(err => response.status(500).send('Server error'))
-        } else // Token is invalid
-            response.status(403).send('Forbidden');
+exports.castVote = ensureLogin((request, response, token) => {
+    if ("id" in request.body && "choice" in request.body) { // Nothing missing
+        let user_id = token.id;
+        let poll_id = request.body.id;
+        let choice = request.body.choice;
+        let docRef = db.collection('polls').doc(poll_id);
+        docRef.get()
+            .then(checkPollAndRequestBallot(response, user_id, choice, docRef))
+            .catch(err => response.status(500).send('Server error'))
     } else // Something missing
         response.status(400).send('Bad request');
-}, ["POST"]);
+});
 
 function checkPollAndRequestBallot(response, user_id, choice, docRef) {
     return doc => {
