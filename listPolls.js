8bb7ba5738fe730b3bf1d2151edafe18272f5465
Brendan Lestander <blestander@pitt.edu>
2020-05-16 10:31:24 -0400

    ListPolls endpoint now provides all owned polls.

--- listPolls.js
+++ listPolls.js
@@ -1,8 +1,24 @@
 const { ensureLogin } = require('./utility.js');
+const { db } = require('./db.js');
 
 exports.listPolls = ensureLogin((request, response, token) => {
-    response.status(200).send([
-        'Hey',
-        'There'
-    ]);
+    db.collection('polls').where('owner', '==', token.id).get()
+        .then(processAndReturnPolls(request, response, token))
+        .catch(err => response.status(500).send('Server error'));
 });
+
+function processAndReturnPolls(request, response, token) {
+    return snapshot => {
+        let results = []
+        snapshot.forEach(doc => {
+            let data = doc.data();
+            let poll = {
+                name: data.name,
+                description: data.description,
+                finished: data.finished
+            }
+            results.push(poll);
+        });
+        response.status(200).send(results);
+    }
+}
