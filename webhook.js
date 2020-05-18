1c60f17b8877b7869e9a505b4227379294651180
Brendan Lestander <blestander@pitt.edu>
2020-05-18 17:20:37 -0400

    Added webhook support for CAV.

--- webhook.js
+++ webhook.js
@@ -29,10 +29,54 @@ function generateWebhookOutput(method, results) {
         case "fptp":
         case "approval":
             return generateSimpleOutput(results);
+        case "cav":
+            return generateCAVOutput(results);
     }
 }
 
 function generateSimpleOutput(results) {
+    // Eliminate empty space in groupings array
+    let prunedGroupings = generatePositiveGroupings(results);
+
+    // Build up lines of output
+    let lines = [];
+    for (let i = 1; i <= prunedGroupings.length; i++)
+        lines.push(
+            `${i}: ${prunedGroupings[prunedGroupings.length - i].join(', ')}`
+                .concat(` - ${results[prunedGroupings[prunedGroupings.length - i][0]]}`)
+        );
+
+    return lines.join('\n');
+}
+
+function generateCAVOutput(results) {
+    let positiveGroupings = generatePositiveGroupings(results);
+
+    // Generate negative groupings
+    let antiResults = {};
+    for (const option in results)
+        antiResults[option] = -results[option];
+    let negativeGroupings = generatePositiveGroupings(antiResults);
+
+    // Build positive lines
+    let lines = []
+    for (let i = 1; i <= positiveGroupings.length; i++)
+        lines.push(
+            `${i}: ${positiveGroupings[positiveGroupings.length - i].join(', ')}`
+                .concat(` - ${results[positiveGroupings[positiveGroupings.length - i][0]]}`)
+        );
+
+    // Build negative lines
+    let isZero = Object.values(results).includes(0);
+    for (let i = isZero ? 1 : 0, j = positiveGroupings.length + 1; i < negativeGroupings.length; i++, j++)
+        lines.push(
+            `${j}: ${negativeGroupings[i].join(', ')} - ${results[negativeGroupings[i][0]]}`
+        );
+
+    return lines.join('\n');
+}
+
+function generatePositiveGroupings(results) {
     let maxVotes = Object.values(results).reduce((previous, current) => Math.max(previous, current), 0);
     let rawGroupings = new Array(maxVotes + 1);
 
@@ -46,14 +90,5 @@ function generateSimpleOutput(results) {
     }
 
     // Eliminate empty space in groupings array
-    let prunedGroupings = rawGroupings.filter(x => Array.isArray(x));
-
-    // Build up lines of output
-    let lines = [];
-    for (let i = 1; i <= prunedGroupings.length; i++)
-        lines.push(
-            `${i}: ${prunedGroupings[prunedGroupings.length - i].join(', ')}`
-        );
-
-    return lines.join('\n');
+    return rawGroupings.filter(x => Array.isArray(x));
 }
