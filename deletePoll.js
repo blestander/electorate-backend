5ec8aa8f87e2d219d405e15f9329f9ddc1b9c86c
Brendan Lestander <blestander@pitt.edu>
2020-05-27 14:20:45 -0400

    Deleting polls now works correctly.

--- deletePoll.js
+++ deletePoll.js
@@ -14,11 +14,32 @@ function validateRequestAndDeletePoll(response, token, docRef) {
             let poll = snapshot.data();
             if (poll.owner == token.id) { // Requester owns poll
                 docRef.delete()
-                    .then(() => response.status(204).end())
-                    .catch(err => response.send(500).send('Server error'));
+                    .then(deleteBallotsAndRespond(response, docRef))
+                    .catch(err => {
+                        console.error(err);
+                        response.status(500).send('Server error');
+                    });
             } else // Requester doesn't own poll and can't delete it
                 response.status(403).send("Cannot delete someone else's poll");
         } else // No such poll
             response.status(404).send('Not found');
+    };
+}
+
+function deleteBallotsAndRespond(response, docRef) {
+    return () => {
+        let batch = db.batch();
+        docRef.collection('ballots').get()
+            .then(snapshot => {
+                snapshot.forEach(doc => {
+                    batch.delete(doc.ref);
+                });
+                batch.commit()
+                    .then(() => response.status(204).end())
+                    .catch(err => {
+                        console.log(err);
+                        response.status(500).send('Server error');
+                    });
+            }).catch(err => console.error(err));
     };
 }
